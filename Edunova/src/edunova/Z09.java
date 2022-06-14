613fcfa99bc9bc4bcebc32e53510b7c5d0210f27
VedranKasapovic <vedran.kasapovic@gmail.com>
2022-06-15 00:13:57 +0200

    Update Z09.java
    dodan ternary

--- Edunova/src/edunova/Z09.java
+++ Edunova/src/edunova/Z09.java
@@ -5,28 +5,33 @@ import javax.swing.JOptionPane;
 public class Z09 {
 	public static void main(String[] args) {
 
-		// program unosi broj između 
+		// program unosi broj između
 		// 1 i 999
 		// U slučaju da je izvan tog raspona
 		// ispisati grešku i prekinuti program
 		// Program ispisuje 1. znamenku upisanog broja
-		
+
 		// -5 greška
 		// 1067 greška
 		// 456 4
 		// 6 6
 		// 89 8
-		
+
 		int a = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
-		if (a<0 || a>1000) {
+		if (a < 0 || a > 1000) {
 			System.out.println("Greška");
-		}else if (a<10) {
+		} else if (a < 10) {
 			System.out.println(a);
-		}else if (a>=10 && a<100) {
-			System.out.println(a/10);
-		}else if (a>=100 && a<1000) {
-			System.out.println(a/100);
+		} else if (a >= 10 && a < 100) {
+			System.out.println(a / 10);
+		} else if (a >= 100 && a < 1000) {
+			System.out.println(a / 100);
 		}
-		
+		System.out.println((a < 0 || a > 1000) ? "Greška"
+				: (a < 10) ? a : (a >= 10 && a < 100) ? a / 10 : (a >= 100 && a < 1000) ? a / 100 : "Greška");
+
+		// ?: Ternary (shorthand for
+		// if-then-else statement)
+
 	}
 }
